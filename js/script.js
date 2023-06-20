333a445051932c69cf7a31e8f8a1575e23a7eb18
DevTom <72770851+Olatomiw@users.noreply.github.com>
2023-06-20 10:33:14 +0100

    All major part of the e-commerce functionality is done at this point

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@
 
 let products = [
     {
+        "id" : 1,
         "name" : "product 1",
         "image": "../images/jhumka-img.png",
         "category" : "jewelry",
@@ -24,6 +25,7 @@ let products = [
         "description": "Orginal Gold Jewellery"
     },
     {
+        "id" : 2,
         "name" : "Kagan Bangles",
         "image" :"../images/kangan-img.png",
         "category" : "jewelry",
@@ -31,6 +33,7 @@ let products = [
         "description": "Orginal Gold Jewellery"
     },
     {
+        "id" : 3,
         "name" : "Necklace",
         "image" : "../images/neklesh-img.png",
         "category" : "jewelry",
@@ -42,25 +45,51 @@ let products = [
 localStorage.setItem("productsData", JSON.stringify(products));
 
 let storedProducts=JSON.parse(localStorage.getItem("productsData"));
-console.log(storedProducts)
-
+// console.log(storedProducts)
+var cartItemsContainer = document.getElementById('cart-items');
 const productContainer = document.getElementById("product-container");
+
+let cart = [];
+console.log("hello")
 storedProducts.forEach((product)=>{
     if(product.category == "jewelry"){
         let productDiv = `<div class=product><h2>${product.name}</h2><img src=${product.image}>
     <p>Price: $${product.price}<p>
     <p>Description: ${product.description}</p>
-    <button>Add to Cart</button></div>`;
-    
-    // document.createElement("div");
-    // productDiv.classList.add("product")
-    // productDiv.innerHTML=`<h2>${product.name}</h2>
-    // <p>Price: $${product.price}<p>
-    // <p>Description: ${product.description}</p>
-    // <button>Add to Cart</button>`;
-
-    // productContainer.innerHTML = ;
-
+    <button class = "addTo" data-name="${product.name}" data-price="${product.price}" data-id="${product.id}">Add to Cart</button></div>`;
     productContainer.innerHTML+=productDiv;
     }
+
+    let addToCartButton = document.querySelectorAll('.addTo');
+    addToCartButton.forEach((button)=>{
+        button.addEventListener('click', function(event) {
+        // console.log("eek")
+        let productName = event.target.dataset.name;
+        let productPrice = event.target.dataset.price;
+        let productId = event.target.dataset.id;
+        let existingItem = cart.find(function(item) {
+            return item.id === productId;
+          });
+          if (existingItem) {
+            alert(`${productName} is already in the cart.`);
+        }
+        else{
+            let productItem = {id:productId, name: productName, price: productPrice };
+            cart.push(productItem);
+        }
+        console.log(cart);
+        // console.log(productItem.id)
+    }) ;   
+    // renderCart();
+  });
 });
+
+function renderCart() {
+    cartItemsContainer.innerHTML = '';
+    cart.forEach(function(item) {
+      var cartItem = document.createElement('li');
+      cartItem.textContent = `${item.name} - $${item.price}`;
+      cartItemsContainer.appendChild(cartItem);
+    });
+  }
+// chatGpt
