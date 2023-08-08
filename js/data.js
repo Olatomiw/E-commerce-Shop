1352b7b4e56fa87e0f2d157ba87208fa76602541
DevTom <72770851+Olatomiw@users.noreply.github.com>
2023-08-08 17:44:05 +0100

    created a new js file for the prodcuts and imported it on the script.js file and also made little changes to the add to cart button

---
+++ js/data.js
@@ -0,0 +1,34 @@
+export let shopProducts = [
+    {
+        "id" : 1,
+        "name" : "product 1",
+        "image": "../images/jhumka-img.png",
+        "category" : "jewelry",
+        "price" : 50,
+        "description": "Orginal Gold Jewellery"
+    },
+    {
+        "id" : 2,
+        "name" : "Kagan Bangles",
+        "image" :"../images/20220218_074203.jpg",
+        "category" : "jewelry",
+        "price" : 50,
+        "description": "Orginal Gold Jewellery"
+    },
+    {
+        "id" : 3,
+        "name" : "Necklace",
+        "image" : "../images/neklesh-img.png",
+        "category" : "jewelry",
+        "price" : 50,
+        "description": "Orginal Gold Jewellery"
+    },
+    {
+        "id" : 4,
+        "name" : "Necklace",
+        "image" : "../images/computer-img.png",
+        "category" : "jewelry",
+        "price" : 50,
+        "description": "Orginal Gold Jewellery"
+    }
+];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+import { shopProducts } from "./data.js";
+let products = shopProducts;
+localStorage.setItem("productsData", JSON.stringify(products));
+let storedProducts=JSON.parse(localStorage.getItem("productsData"));
+// console.log(storedProducts)
+var cartItemsContainer = document.getElementById('cart-items');
+const productContainer = document.getElementById("product-container");
+
 // To add background to the cart logo at the top when users s
 window.onscroll = function(){myScroll()};
 function myScroll (){
@@ -8,49 +16,6 @@ function myScroll (){
         document.getElementById("E-logo").className="logo-cart";
     }
 }
-let products = [
-    {
-        "id" : 1,
-        "name" : "product 1",
-        "image": "../images/jhumka-img.png",
-        "category" : "jewelry",
-        "price" : 50,
-        "description": "Orginal Gold Jewellery"
-    },
-    {
-        "id" : 2,
-        "name" : "Kagan Bangles",
-        "image" :"../images/20220218_074203.jpg",
-        "category" : "jewelry",
-        "price" : 50,
-        "description": "Orginal Gold Jewellery"
-    },
-    {
-        "id" : 3,
-        "name" : "Necklace",
-        "image" : "../images/neklesh-img.png",
-        "category" : "jewelry",
-        "price" : 50,
-        "description": "Orginal Gold Jewellery"
-    },
-    {
-        "id" : 4,
-        "name" : "Necklace",
-        "image" : "../images/computer-img.png",
-        "category" : "jewelry",
-        "price" : 50,
-        "description": "Orginal Gold Jewellery"
-    }
-];
-
-localStorage.setItem("productsData", JSON.stringify(products));
-
-let storedProducts=JSON.parse(localStorage.getItem("productsData"));
-// console.log(storedProducts)
-var cartItemsContainer = document.getElementById('cart-items');
-const productContainer = document.getElementById("product-container");
-
-
 let cart = [];
 console.log("hello")
 // Creating product on the html with js
@@ -61,12 +26,15 @@ storedProducts.forEach((product)=>{
         <div class="img-cont"><img src=${product.image}></div>
         <p>Price: $${product.price}<p>
         <p>Description: ${product.description}</p>
-        <button class = "addTo" data-name="${product.name}" data-image="${product.image}" data-price="${product.price}" data-id="${product.id}">Add to Cart</button>
-        </div>`;
+        <button class = "addTo CartBtn" data-name="${product.name}" data-image="${product.image}" data-price="${product.price}" data-id="${product.id}"> <span class="IconContainer"> 
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(252, 251, 251)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
+      </span>
+      <p class="text" data-name="${product.name}" data-image="${product.image}" data-price="${product.price}" data-id="${product.id}">Add to Cart</p></button>
+    </div>`;
     productContainer.innerHTML+=productDiv;
     }
         // Add product to cart
-    let addToCartButton = document.querySelectorAll('.addTo');
+    let addToCartButton = document.querySelectorAll('.CartBtn');
     addToCartButton.forEach((button)=>{
         button.addEventListener('click', function(event) {
         // console.log("eek")
@@ -96,15 +64,16 @@ function renderCart() {
         var productT = 0
         let productTotal = document.querySelector("#cart-Total")
         cart.forEach(function(item, index) {
+            let newPrice = 2 * item.price;
             var cartItem = document.createElement('li');
             cartItem.innerHTML = `<div class="cart-list">
                 <div class="cart-img"><img src="${item.img}"></div>
-                <p>${item.name}</p> - <p>$${item.price}</p>
-                <button class="delete-item" data-index="${index}">x</button>
+                <div><p>${item.name}</p></div> - <div><p>$${newPrice}</p></div>
+                <div><button class="delete-item" data-index="${index}">x</button></div>
             </div>`;
             cartItemsContainer.appendChild(cartItem);
             // total of price
-            let toNum = parseFloat(item.price)
+            let toNum = parseFloat(newPrice)
             productT +=toNum;
         });
         productTotal.innerHTML=`Total:$${productT}`;
