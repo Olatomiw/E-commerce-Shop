e06f91ae83f050ba92d5eb8e56754feeb758abcf
DevTom <72770851+Olatomiw@users.noreply.github.com>
2023-08-10 14:27:25 +0100

    changed the banner image and added more product to my work

--- js/data.js
+++ js/data.js
@@ -1,32 +1,64 @@
 export let shopProducts = [
     {
         "id" : 1,
-        "name" : "product 1",
-        "image": "../images/jhumka-img.png",
+        "name" : "Alienware Computer",
+        "image": "../images/alienware-ickPGVMMr98-unsplash.jpg",
         "category" : "jewelry",
         "price" : 50,
-        "description": "Orginal Gold Jewellery"
+        "description": "Alienware Computer"
     },
     {
         "id" : 2,
-        "name" : "Kagan Bangles",
-        "image" :"../images/20220218_074203.jpg",
+        "name" : "JBL Speaker",
+        "image" :"../images/sebastian-scholz-nuki-Fh3Dtg6QX4Q-unsplash.jpg",
         "category" : "jewelry",
         "price" : 50,
         "description": "Orginal Gold Jewellery"
     },
     {
         "id" : 3,
-        "name" : "Necklace",
-        "image" : "../images/neklesh-img.png",
+        "name" : "Samsung Tablet",
+        "image" : "../images/henry-ascroft-7OFnb7NOvjw-unsplash.jpg",
+        "category" : "jewelry",
+        "price" : 50,
+        "description": "Samsung Tablet 10gb RAM"
+    },
+    {
+        "id" : 4,
+        "name" : "Camera",
+        "image" : "../images/chuttersnap-CuLzFor2c4M-unsplash.jpg",
         "category" : "jewelry",
         "price" : 50,
         "description": "Orginal Gold Jewellery"
     },
     {
-        "id" : 4,
-        "name" : "Necklace",
-        "image" : "../images/computer-img.png",
+        "id" : 5,
+        "name" : "Acer Laptop",
+        "image" : "../images/Gadget-PNG-Transparent-Image.png",
+        "category" : "jewelry",
+        "price" : 50,
+        "description": "Orginal Gold Jewellery"
+    },
+    {
+        "id" : 5,
+        "name" : "Headphone",
+        "image" : "../images/c-d-x-PDX_a_82obo-unsplash.jpg",
+        "category" : "jewelry",
+        "price" : 50,
+        "description": "Orginal Gold Jewellery"
+    },
+    {
+        "id" : 7,
+        "name" : "Apple watch",
+        "image" :"../images/daniel-korpai-wW7XbWYoqK8-unsplash.jpg",
+        "category" : "jewelry",
+        "price" : 50,
+        "description": "Orginal Gold Jewellery"
+    },
+    {
+        "id" : 8,
+        "name" : "Dell Laptop",
+        "image" :"../images/Gadget-PNG-Transparent-HD-Photo.png",
         "category" : "jewelry",
         "price" : 50,
         "description": "Orginal Gold Jewellery"
